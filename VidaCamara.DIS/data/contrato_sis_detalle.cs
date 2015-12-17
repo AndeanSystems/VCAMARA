f77e1f00f0f5b7503a24d9b4b0980498a0aff1e9
david <choquerojasnoedavid@gmail.com>
2015-12-16 23:40:01 -0500

    clase contrato detalle sis

---
+++ VidaCamara.DIS/data/contrato_sis_detalle.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VidaCamara.DIS.Modelo;
+
+namespace VidaCamara.DIS.data
+{
+    public class contrato_sis_detalle
+    {
+        public List<CONTRATO_DETALLE> getlistContratoDetalle(){
+            try
+            {
+                using (var db = new DISEntities())
+                {
+                    return db.CONTRATO_DETALLEs.ToList();
+                }
+            }
+            catch (Exception)
+            {
+                
+                throw;
+            }
+        }
+    }
+}
