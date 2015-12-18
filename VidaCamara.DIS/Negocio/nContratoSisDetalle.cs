07382c85e0a37bf75c3030e2f32b4c05e1e0277b
davidrojas <juxirolove_94@hotmail.com>
2015-12-18 10:36:01 -0500

    negocio contrato detalle

---
+++ VidaCamara.DIS/Negocio/nContratoSisDetalle.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using VidaCamara.DIS.data;
+using VidaCamara.DIS.Modelo;
+
+namespace VidaCamara.DIS.Negocio
+{
+    public class nContratoSisDetalle
+    {
+        public List<CONTRATO_SIS_DET> getlistContratoDetalle()
+        {
+            return new dContrato_sis_detalle().getlistContratoDetalle();
+        }
+        public Int32 setGuardarContratoDetalle(CONTRATO_SIS_DET det)
+        {
+            return new dContrato_sis_detalle().setGuardarContratoDetalle(det);
+        }
+        public Int32 setActualizarContratoDetalle(CONTRATO_SIS_DET det)
+        {
+            return new dContrato_sis_detalle().setActualizarContratoDetalle(det);
+        }
+    }
+}
