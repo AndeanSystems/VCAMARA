a84ce1fe841a27237dd0d56fa7162ecf1f328e58
davidrojas <juxirolove_94@hotmail.com>
2015-12-18 20:40:44 -0500

    AGREGADO DE FUNCION NUMERIC

--- VidaCamara.Web/Resources/js/ConfirmWindow.js
+++ VidaCamara.Web/Resources/js/ConfirmWindow.js
@@ -46,6 +46,7 @@ $(document).ready(function () {
     //funcion datepicker
     $.datepicker.setDefaults($.datepicker.regional['es']);
     $(".datetime").datepicker();
+    (".numeric").numeric();
     $('#ctl00_ContentPlaceHolder1_txt_fecini_c').datepicker();
     $('#ctl00_ContentPlaceHolder1_txt_fecfin_c').datepicker();
     $('#ctl00_ContentPlaceHolder1_txt_fecha_creacion').datepicker();
