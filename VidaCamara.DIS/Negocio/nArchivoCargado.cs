93653a15441f371145230c5c161c30d475eb70c9
davidrojas <juxirolove_94@hotmail.com>
2015-12-22 23:49:28 -0500

    capa negocio de consulta datos

---
+++ VidaCamara.DIS/Negocio/nArchivoCargado.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VidaCamara.DIS.Modelo;
+using VidaCamara.DIS.data;
+
+namespace VidaCamara.DIS.Negocio
+{
+    public class nArchivoCargado
+    {
+        public List<HistorialCargaArchivo_LinDet> listArchivoCargado(HistorialCargaArchivo_LinCab historiaLinCab, object[] filterParam)
+        {
+            return new dPagoCargado().listArchivoCargado(historiaLinCab,filterParam);
+        }
+    }
+}
