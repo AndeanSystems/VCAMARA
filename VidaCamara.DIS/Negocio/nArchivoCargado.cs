db655bee50d59be53c220a71924a603821b904df
david <choquerojasnoedavid@gmail.com>
2015-12-24 13:33:40 -0500

    negocio caraga archivo

--- VidaCamara.DIS/Negocio/nArchivoCargado.cs
+++ VidaCamara.DIS/Negocio/nArchivoCargado.cs
@@ -10,9 +10,9 @@ namespace VidaCamara.DIS.Negocio
 {
     public class nArchivoCargado
     {
-        public List<HistorialCargaArchivo_LinDet> listArchivoCargado(HistorialCargaArchivo_LinCab historiaLinCab, object[] filterParam)
+        public List<HistorialCargaArchivo_LinDet> listArchivoCargado(HistorialCargaArchivo_LinCab historiaLinCab,HistorialCargaArchivo_LinDet historiaLinDet, object[] filterParam,int jtStartIndex, int jtPageSize,out int total)
         {
-            return new dPagoCargado().listArchivoCargado(historiaLinCab,filterParam);
+            return new dPagoCargado().listArchivoCargado(historiaLinCab,historiaLinDet, filterParam, jtStartIndex, jtPageSize,out total);
         }
     }
 }
