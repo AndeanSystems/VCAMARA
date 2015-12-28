2709ac407ae3ca05174a537ba495ed89c23ddc78
davidrojas <juxirolove_94@hotmail.com>
2015-12-27 23:27:36 -0500

    CREACION DE archivo data

---
+++ VidaCamara.DIS/data/dArchivo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using VidaCamara.DIS.Modelo;
+
+namespace VidaCamara.DIS.data
+{
+    public class dArchivo
+    {
+        public List<Archivo> listExisteArchivo(Archivo archivo)
+        {
+            try
+            {
+                using (var db = new DISEntities())
+                {
+                    return db.Archivos.Where(a => a.NombreArchivo.Equals(archivo.NombreArchivo)).ToList();
+                }
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+        }
+    }
+}
