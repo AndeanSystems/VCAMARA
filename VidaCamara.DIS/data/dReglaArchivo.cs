d9b3152df948989adfadd93dca7d60c7f2885ec3
davidrojas <juxirolove_94@hotmail.com>
2015-12-27 23:28:36 -0500

    creacion de data para regla archivo

---
+++ VidaCamara.DIS/data/dReglaArchivo.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VidaCamara.DIS.Modelo;
+
+namespace VidaCamara.DIS.data
+{
+    public class dReglaArchivo
+    {
+        public List<ReglaArchivo> getListReglaArchivo(ReglaArchivo regla)
+        {
+            try
+            {
+                using (var db = new DISEntities())
+                {
+                    return db.ReglaArchivos.Where(a=>a.Archivo.Equals(regla.Archivo) && (a.TipoLinea.Equals(regla.TipoLinea) || regla.TipoLinea.Equals("0"))).ToList();
+                }
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+        }
+    }
+}
