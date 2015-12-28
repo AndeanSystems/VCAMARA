708f14abe2425985c5ce2bd3aca438241e6b088d
davidrojas <juxirolove_94@hotmail.com>
2015-12-27 23:30:26 -0500

    creacion de negocio para archivo

---
+++ VidaCamara.DIS/Negocio/nArchivo.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using VidaCamara.DIS.data;
+using VidaCamara.DIS.Modelo;
+
+namespace VidaCamara.DIS.Negocio
+{
+    public class nArchivo
+    {
+        public List<Archivo> listExisteArchivo(Archivo archivo)
+        {
+            return new dArchivo().listExisteArchivo(archivo);
+        }
+    }
+}
