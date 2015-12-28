1f0637bb0712cf2776c1c1bf57b94ace018127cf
davidrojas <juxirolove_94@hotmail.com>
2015-12-27 23:30:55 -0500

    creacion de negocio para entidad regla archivo

---
+++ VidaCamara.DIS/Negocio/nReglaArchivo.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using VidaCamara.DIS.Modelo;
+using VidaCamara.DIS.data;
+
+namespace VidaCamara.DIS.Negocio
+{
+    public class nReglaArchivo
+    {
+        public List<ReglaArchivo> getListReglaArchivo(ReglaArchivo regla)
+        {
+            return new dReglaArchivo().getListReglaArchivo(regla);
+        }
+    }
+}
