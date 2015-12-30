e718465b4c1a05ed61e3a0f5a17b63e25c3f90d3
davidrojas <juxirolove_94@hotmail.com>
2015-12-30 00:07:27 -0500

    REGENERACION DEL MODELO 30122015

--- VidaCamara.DIS/data/dContratoSis.cs
+++ VidaCamara.DIS/data/dContratoSis.cs
@@ -1,11 +1,14 @@
-﻿using System.Collections.Generic;
-using VidaCamara.DIS.Modelo;
+﻿using VidaCamara.DIS.Modelo;
 using System.Linq;
 
 namespace VidaCamara.DIS.data
 {
     public class dContratoSis
     {
+        //INVOCAR STORE PROCEDURE
+        //var date1 = DateTime.Now;
+        //var date2 = DateTime.Now;
+        //db.pa_Valida_RangoFechaXContrato(date1, date2);
         public CONTRATO_SYS listContratoByID(CONTRATO_SYS contrato) {
             try
             {
