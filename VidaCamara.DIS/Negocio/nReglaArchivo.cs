4d1642f814f3e915074ab69fe273ef81f540e799
davidrojas <juxirolove_94@hotmail.com>
2015-12-31 20:43:30 -0500

    Se creo un metodo el cual crea las columnas de la grilla dependiendo la regla de archivo x tipo de archivo

--- VidaCamara.DIS/Negocio/nReglaArchivo.cs
+++ VidaCamara.DIS/Negocio/nReglaArchivo.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using VidaCamara.DIS.Modelo;
 using VidaCamara.DIS.data;
+using System.Text;
 
 namespace VidaCamara.DIS.Negocio
 {
@@ -9,6 +10,25 @@ namespace VidaCamara.DIS.Negocio
         public List<ReglaArchivo> getListReglaArchivo(ReglaArchivo regla, int jtStartIndex, int jtPageSize, out int total)
         {
             return new dReglaArchivo().getListReglaArchivo(regla, jtStartIndex, jtPageSize, out total);
+        }
+        /// <summary>
+        /// Devuelve la las colunas para una grilla en formato de json
+        /// </summary>
+        /// <param name="regla"></param>
+        /// <returns></returns>
+        public StringBuilder getColumnGridByArchivo(ReglaArchivo regla)
+        {
+            var total = 0;
+            var listRegla = new dReglaArchivo().getListReglaArchivo(regla, 0, 200, out total);
+            var sb = new StringBuilder();
+            sb.Append("var fields = {");
+            for (int i = 1; i <= listRegla.Count; i++)
+            {
+                sb.Append(listRegla[i - 1].NombreCampo + ":{");
+                sb.Append("title:" + "'" + listRegla[i - 1].TituloColumna + "'}" + (i == listRegla.Count ? "" : ","));
+            }
+            sb.Append("};");
+            return sb;
         }
     }
 }
