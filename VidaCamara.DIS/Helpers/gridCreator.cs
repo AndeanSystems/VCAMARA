d18bd6527896cafec1b804d54b45cba816f5bcb1
davidrojas <juxirolove_94@hotmail.com>
2015-12-31 20:42:05 -0500

    Crea y devuelve jtable en formaro de json

---
+++ VidaCamara.DIS/Helpers/gridCreator.cs
@@ -0,0 +1,25 @@
+﻿using System.Text;
+
+namespace VidaCamara.DIS.Helpers
+{
+    public class gridCreator
+    {
+        public StringBuilder getGrid(string id,string size,string action,string sorting)
+        {
+            var sb = new StringBuilder();
+            sb.Append("$(document).ready(function () {");
+            sb.Append("$('#"+id+"').jtable({");
+            sb.Append("paging: true,");
+            sb.Append("sorting: true,");
+            sb.Append("pageSize: 13,");
+            sb.Append("defaultSorting: '"+sorting+"',");
+            sb.Append("actions: {listAction: '" + action + "'},");
+            sb.Append("fields:fields");
+            sb.Append("});");
+            sb.Append("$('#"+id+".jtable-main-container').css({'width':'"+size+"px'});");
+            sb.Append("$('#"+id+"').jtable('load');");
+            sb.Append("});");
+            return sb;
+        }
+    }
+}
