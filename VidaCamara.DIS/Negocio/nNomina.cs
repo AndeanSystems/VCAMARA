7376d340de2cc8adedec1b6c40ceadd49107905c
david <choquerojasnoedavid@gmail.com>
2016-01-03 14:19:28 -0500

    clase nomina

---
+++ VidaCamara.DIS/Negocio/nNomina.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VidaCamara.DIS.data;
+using VidaCamara.DIS.Modelo;
+
+namespace VidaCamara.DIS.Negocio
+{
+    public class nNomina
+    {
+        public Int32 setGrabarNomina(NOMINA nomina) 
+        {
+            return new dNomina().setGrabarNomina(nomina);
+        }
+    }
+}
