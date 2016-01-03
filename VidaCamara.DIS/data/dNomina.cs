d56c70d6ce2825a23a3fc5fd01f63e14207e3634
david <choquerojasnoedavid@gmail.com>
2016-01-03 14:19:10 -0500

    Nomina

---
+++ VidaCamara.DIS/data/dNomina.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VidaCamara.DIS.Modelo;
+
+namespace VidaCamara.DIS.data
+{
+    public class dNomina
+    {
+        public Int32 setGrabarNomina(NOMINA nomina) {
+            try
+            {
+                using (var db = new DISEntities()) 
+                {
+                    db.NOMINAs.Add(nomina);
+                    return db.SaveChanges();
+                }
+            }
+            catch (Exception ex)
+            {
+                
+                throw;
+            }
+        }
+    }
+}
