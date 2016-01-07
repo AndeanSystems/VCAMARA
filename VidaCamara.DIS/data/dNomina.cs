96bab715f6c3ec328349410f988e3182c2cf3f59
davidrojas <juxirolove_94@hotmail.com>
2016-01-06 20:49:36 -0500

    lista nominas

--- VidaCamara.DIS/data/dNomina.cs
+++ VidaCamara.DIS/data/dNomina.cs
@@ -67,5 +67,53 @@ namespace VidaCamara.DIS.data
                 throw;
             }
         }
+
+        public List<NOMINA> listNominaConsulta(NOMINA nomina, object[] filters, int jtStartIndex, int jtPageSize, out int total)
+        {
+            var listNomina = new List<NOMINA>();
+            try
+            {
+                using (var db = new DISEntities())
+                {
+                    var nombreTipoArchivo = filters[0].ToString();
+                    var query = (from a in db.NOMINAs
+                                 join b in db.Archivos on
+                                 a.ArchivoId equals b.ArchivoId
+                                 join c in db.TipoArchivos on
+                                 b.TipoArchivoId equals c.TipoArchivoId
+                                 where
+                                    c.NombreTipoArchivo == nombreTipoArchivo
+                                &&  a.IDE_CONTRATO == nomina.IDE_CONTRATO
+                                select a).ToList();
+                    total = query.Count();
+                    foreach (var item in query.Skip(jtStartIndex).Take(jtPageSize))
+                    {
+                        var eNomina = new NOMINA() {
+                            Id_Nomina = item.Id_Nomina,
+                            ArchivoId = item.ArchivoId,
+                            IDE_CONTRATO = item.IDE_CONTRATO,
+                            RUC_ORDE = item.RUC_ORDE,
+                            CTA_ORDE = item.CTA_ORDE,
+                            COD_TRAN = item.COD_TRAN,
+                            TIP_MONE = item.TIP_MONE,
+                            MON_TRAN = item.MON_TRAN,
+                            FEC_TRAN = item.FEC_TRAN,
+                            RUC_BENE = item.RUC_BENE,
+                            NOM_BENE = item.NOM_BENE,
+                            TIP_CTA = item.TIP_CTA,
+                            CTA_BENE = item.CTA_BENE,
+                            DET_TRAN = item.DET_TRAN
+                        };
+                        listNomina.Add(eNomina);
+                    }
+                }
+                return listNomina;
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+        }
     }
 }
