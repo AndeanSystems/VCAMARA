bf62b6c39ae9bb1e3442e26a1591af23617e4213
david <choquerojasnoedavid@gmail.com>
2016-01-08 19:41:35 -0500

    agreqgdo de funciones

--- VidaCamara.Web/WebPage/ModuloDIS/Operaciones/js/CargaDatos.js
+++ VidaCamara.Web/WebPage/ModuloDIS/Operaciones/js/CargaDatos.js
@@ -6,17 +6,11 @@
         var tipo_archivo = $("#ctl00_ContentPlaceHolder1_ddl_tipo_archivo").val();
         var tipo_archivo_des = $("#ctl00_ContentPlaceHolder1_ddl_tipo_archivo option:selected").text();
         if (parseInt(nro_contrato) == 0) {
-            MessageBox("Seleccione el contrato"); return false;
+            mostrarMensajeAlert("Seleccione el contrato"); return false;
         } else if (parseInt(tipo_archivo) == 0) {
-            MessageBox("Seleccione el tipo de archivo"); return false;
+            mostrarMensajeAlert("Seleccione el tipo de archivo"); return false;
         } else {
             return confirm("¿ Esta seguro de Guardar el tipo de archivo " + tipo_archivo_des + " ?");
         }
     });
-
-    function MessageBox(texto) {
-        $("<div style='font-size:14px;text-align:center;'>" + texto + "</div>").dialog({ title: 'Alerta', modal: true, width: 400, height: 160, buttons: [{ id: 'aceptar', text: 'Aceptar', icons: { primary: 'ui-icon-circle-check' }, click: function () { $(this).dialog('close'); } }] })
-    }
-    //asigncion de funciones jquery 
-    //$("#ctl00_ContentPlaceHolder1_txt_mesvig_d").datepicker();
 });
