293f9a16a54398010e3418bcf061b9555b2aad36
davidrojas <juxirolove_94@hotmail.com>
2016-01-09 19:34:32 -0500

    Creacion de nuevas entidades

---
+++ VidaCamara.DIS/Negocio/nSegDescarga.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VidaCamara.DIS.data;
+using VidaCamara.DIS.Modelo.EEntidad;
+
+namespace VidaCamara.DIS.Negocio
+{
+    public class nSegDescarga
+    {
+        public List<ESegDescarga> listSegDescarga()
+        {
+            return new dSegDescarga().listSegDescarga();
+        }
+    }
+}
---
+++ VidaCamara.DIS/data/dSegDescarga.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using VidaCamara.DIS.Modelo;
+using VidaCamara.DIS.Modelo.EEntidad;
+
+namespace VidaCamara.DIS.data
+{
+    public class dSegDescarga
+    {
+        public List<ESegDescarga> listSegDescarga()
+        {
+            try
+            {
+                using (var db = new DISEntities())
+                {
+                    return new List<ESegDescarga>();
+                }
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+        }
+    }
+}
