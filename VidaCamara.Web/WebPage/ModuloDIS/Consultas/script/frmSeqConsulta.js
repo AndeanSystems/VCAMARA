418f2a956b2820bccdd1ef74267ed45bacc5327b
david <choquerojasnoedavid@gmail.com>
2016-06-02 23:01:56 -0500

    Cambios 2 06 2016

--- VidaCamara.Web/WebPage/ModuloDIS/Consultas/script/frmSeqConsulta.js
+++ VidaCamara.Web/WebPage/ModuloDIS/Consultas/script/frmSeqConsulta.js
@@ -6,17 +6,17 @@
         if (parseInt(contrato) == 0) {
             mostrarMensajeAlert("Seleccione Contrato"); return false;
         }
-        $("#frmOperacion select").each(function (index, element) {
-            if (element.value != "0")
-                filterSelected++;
-        });
-        $("#frmOperacion input[type='text']").each(function (index, element) {
-            if (element.value != "") {
-                filterSelected++;
-            }
-        });
-        if (filterSelected < 2) {
-            mostrarMensajeAlert("Seleccione como minimo 2 o más campos"); return false;
-        }
+        ////$("#frmOperacion select").each(function (index, element) {
+        ////    if (element.value != "0")
+        ////        filterSelected++;
+        ////});
+        ////$("#frmOperacion input[type='text']").each(function (index, element) {
+        ////    if (element.value != "") {
+        ////        filterSelected++;
+        ////    }
+        ////});
+        ////if (filterSelected < 2) {
+        ////    mostrarMensajeAlert("Seleccione como minimo 2 o más campos"); return false;
+        ////}
     });
 })
